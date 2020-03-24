c9121e713808341370414aae82522fab5ccaf462
Lucas Lima <lucas.developmaster@gmail.com>
2020-03-24 00:25:33 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        async function fetchData () {
+            const response = await fetch('https://api.github.com/users/lucaslz/repos');
+            const data = await response.json();
+            setRepositories(data);
+        }
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const favoriteRepositories = repositories.filter( repo => repo.favorite );
+
+        document.title = `Você tem ${favoriteRepositories.length} favoritos!`;
+    }, [repositories]);
+
+    function handleClickFavoritar(id) {
+        const newRepositories = repositories.map( repo => {
+            return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+        });
+        setRepositories(newRepositories);
+    }
+
+    return (
+        <ul>
+            {repositories.map(repo => (
+                <li key={repo.id}>
+                    {repo.name} |&nbsp;
+                    {repo.favorite && <span>Favorito |&nbsp;</span>}
+                    <button onClick={() => handleClickFavoritar(repo.id)}>Favoritar</button>
+                </li>
+            ))}
+        </ul>
+    );
 }
-
-export default App;
